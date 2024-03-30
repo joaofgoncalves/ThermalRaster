e79f7fe786073e71ec20dea98f6b0ae8ea364336
João Gonçalves <joaofgo@gmail.com>
2024-03-30 18:03:03 +0000

    Updated welcome message

--- R/zzz.R
+++ R/zzz.R
@@ -1,7 +1,17 @@
-
+#' Package Attachment Welcome Message
+#'
+#' Displays a welcome message with the current package version when the package is loaded.
+#'
+#' @param libname The library path of the attached package (automatically provided by R).
+#' @param pkgname The name of the package being attached (automatically provided by R).
+#'
+#' @keywords internal
+#' @importFrom utils packageVersion
+#' @noRd
+#'
 .onAttach <- function(libname, pkgname) {
   # Retrieve the package version
-  version <- as.character(packageVersion("ThermalRaster"))
+  version <- as.character(utils::packageVersion("ThermalRaster"))
 
   # Create a custom message with the package version
   message <- paste0("Welcome to ThermalRaster version: ", version, "!")
